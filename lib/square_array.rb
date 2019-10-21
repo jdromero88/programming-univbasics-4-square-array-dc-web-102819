e92478483b4987cb5a6258e6c05c0733b7ab3fad
José Romero <jdromerov88@gmail.com>
2019-10-21 00:30:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,10 @@
 def square_array(array)
   # your code here
   i = 0
+  new_arreglo
   while i < array.lenght do
     new_arreglo[i] = array[i]**2
-    i++
+    i += 1
   end
     puts new_arreglo
 end
